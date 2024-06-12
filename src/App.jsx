720b95281220df4231e8219a0ee0a166e62bd104
Camilla <milla_mendes83@hotmail.com>
2024-06-12 17:49:51 -0300

    Inciando estudos sobre Styled Components e desenvolvimento do Space App

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+import EstilosGlobais from "./Components/EstilosGlobais";
+import Cabecalho from "./Components/Cabecalho";
+
+const FundoGradiente = styled.div`
+  background: linear-gradient(174.61deg, #041833 4.16%, #04244F 48%, #154580 96.76%);
+  width: 100%;
+  min-height: 100vh;
+  margin: 0;
+` 
+
+function App() {
+
+  return (
+    <>
+    <FundoGradiente>
+      <EstilosGlobais />
+      <Cabecalho />
+    </FundoGradiente>
+    </>
+      
+  )
+}
+
+export default App;
