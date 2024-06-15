00917a1cc37cd686476d0868d869c34357618489
Camilla <milla_mendes83@hotmail.com>
2024-06-15 01:12:37 -0300

    Montando o layout desktop

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import styled from "styled-components";
 import EstilosGlobais from "./Components/EstilosGlobais";
 import Cabecalho from "./Components/Cabecalho";
 import BarraLateral from "./Components/BarraLateral";
+import Banner from "./Components/Banner";
+import banner from "./assets/FotoBanner.png"
+import Galeria from "./Components/Galeria";
 
 const FundoGradiente = styled.div`
   background: linear-gradient(174.61deg, #041833 4.16%, #04244F 48%, #154580 96.76%);
@@ -10,13 +13,41 @@ const FundoGradiente = styled.div`
   margin: 0;
 ` 
 
+const AppContainer = styled.div`
+  width: 1440px;
+  max-width: 100%;
+  margin: 0 auto;
+`
+
+const MainContainer = styled.main`
+  display: flex;
+  gap: 24px;
+`
+
+const ConteudoGaleria = styled.section`
+  display: flex;
+  flex-direction: column;
+  flex-grow: 1;
+`
+
 function App() {
 
   return (
     <FundoGradiente>
       <EstilosGlobais />
-      <Cabecalho />
-      <BarraLateral />
+      <AppContainer>
+        <Cabecalho />
+        <MainContainer>
+          <BarraLateral />
+          <ConteudoGaleria>
+            <Banner
+              texto="A galeria mais completa de fotos do espaço!"
+              backgroundImage={banner}
+            />
+            <Galeria />
+          </ConteudoGaleria>
+        </MainContainer>
+      </AppContainer>
     </FundoGradiente>
   )
 }
---
+++ src/Components/Galeria/index.jsx
@@ -0,0 +1,6 @@
+
+const Galeria = () => {
+  return <h2>Navegue pela galeria</h2>
+}
+
+export default Galeria;
